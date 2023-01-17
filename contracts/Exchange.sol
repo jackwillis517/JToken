fbce04c6ce82b7faf5cd65f3ecca5567dae3895a
Jack Willis <jackwillis517@gmail.com>
2023-01-17 13:32:13 -0500

    Fixed the testing bug and finished testing the sell function.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -5,10 +5,10 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Exchange {
     IERC20 public token;
-    uint price;
+    uint256 price;
     address owner;
 
-    constructor(IERC20 _token, uint _price){
+    constructor(IERC20 _token, uint256 _price) {
         token = _token;
         price = _price;
         owner = msg.sender;
@@ -16,35 +16,49 @@ contract Exchange {
 
     function giveExchangeTokens() external {
         require(msg.sender == owner);
-        uint allowance = token.allowance(msg.sender, address(this));
-        token.transferFrom(msg.sender, address(this), allowance);
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        bool sent = token.transferFrom(msg.sender, address(this), allowance);
+        require(sent, "Failed to send tokens to the exchange");
     }
 
-    function sell(uint amount) external payable {
-        uint allowance = token.allowance(msg.sender, address(this));
-        require(token.balanceOf(msg.sender) >= amount, "You don't have that many tokens to sell.");
-        require(allowance == amount, "You didn't permit the exchange to take this many tokens from you.");
-        bool tokensTaken = token.transferFrom(msg.sender, address(this), allowance);
+    function sell(uint256 amount) external payable {
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(
+            token.balanceOf(msg.sender) >= amount,
+            "You don't have that many tokens to sell."
+        );
+        require(
+            allowance == amount,
+            "You didn't permit the exchange to take this many tokens from you."
+        );
+        bool tokensTaken = token.transferFrom(
+            msg.sender,
+            address(this),
+            allowance
+        );
         assert(tokensTaken);
         (bool sent, ) = payable(msg.sender).call{value: viewPrice(amount)}("");
         assert(sent);
     }
 
-    function buy(uint amount) external payable {
-        require(token.balanceOf(address(this)) >= amount, "Not enough tokens left.");
+    function buy(uint256 amount) external payable {
+        require(
+            token.balanceOf(address(this)) >= amount,
+            "Not enough tokens left."
+        );
         require(msg.value == viewPrice(amount), "Insufficent funds.");
         token.transfer(msg.sender, amount);
     }
 
-    function viewPrice(uint amount) public view returns (uint) {
+    function viewPrice(uint256 amount) public view returns (uint256) {
         return amount * price;
     }
 
-    function viewApproved() external view returns (uint) {
+    function viewApproved() external view returns (uint256) {
         return token.allowance(msg.sender, address(this));
     }
 
-    function viewBalance() external view returns (uint) {
+    function viewBalance() external view returns (uint256) {
         return token.balanceOf(msg.sender);
     }
 }
--- test/ExchangeTest.js
+++ test/ExchangeTest.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Exchange", function () {
-  let supply = 100;
+  let supply = 100000;
   let cost = 1000;
   let token;
   let exchange;
@@ -19,21 +19,46 @@ describe("Exchange", function () {
     exchange = await Exchange.deploy(token.address, cost);
   });
 
+  describe("Give Exchange Token", () => {
+    it("Should approve the exchange to take all of the tokens", async () => {
+      await token.approve(exchange.address, 100000);
+    });
+
+    it("Should give all tokens from the owner to the exchange", async () => {
+      await expect(exchange.giveExchangeTokens()).to.changeTokenBalances(
+        token,
+        [owner.address, exchange.address],
+        [-100000, 100000]
+      );
+    });
+  });
+
   describe("Sell", () => {
+    it("Should allow addr1 to buy one", async () => {
+      await exchange.connect(addr1).buy(1, { value: 1000 });
+    });
+
     it("Should fail if the sender didn't approve their tokens to be taken", async () => {
-        await expect(exchange.connect(addr1).sell(1)).to.be.reverted;
+      await expect(exchange.connect(addr1).sell(1)).to.be.reverted;
     });
 
-    it("Should taken", async () => {
-        await token.connect(owner).approve(exchange.address, 1000);
-        await exchange.connect(owner).giveExchangeTokens();
+    it("Should allow the user to approve the selling of a token on the exchange", async () => {
+      await token.connect(addr1).approve(exchange.address, 1);
     });
 
-    // it("Should allow the user to approve the selling of a token on the exchange", async () => {
-    //     await exchange.connect(addr1).buy(1, { value: 1000 });
-    //     await token.connect(addr1).approve(exchange.address, 1);
-    //     const approvedAmount = await exchange.connect(addr1).viewApproved();
-    //     expect(parseInt(approvedAmount)).to.equal(1);
-    // });
+    it("Should allow the user to sell a token back to the exchange", async () => {
+      await expect(exchange.connect(addr1).sell(1)).to.changeTokenBalances(
+        token,
+        [exchange.address, addr1.address],
+        [1, -1]
+      );
+    });
+  });
+
+  describe("Buy", () => {
+    it("Should reject if the proper amount of eth is included in the transaction", async () => {
+      await expect(exchange.connect(addr1).buy(1, { value: 100 })).to.be
+        .reverted;
+    });
   });
 });
